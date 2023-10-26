383f34585c5719a47acc59fc608145bc8c1d6453
Brandon Oliver <brandon.oliverlubiano@cesunbc.edu.mx>
2023-10-26 11:36:52 -0700

    Script para automatizar la conexion de red a Labelnet

---
+++ adm.bat
@@ -0,0 +1,14 @@
+netsh wlan add filter permission=allow ssid="labelnet1_admins" networktype=infrastructure
+netsh wlan add profile filename="D:\labelnet1.xml" interface="Wi-Fi"
+netsh wlan show profile
+netsh wlan connect ssid=labelnet1_admins name=labelnet1_admins
+
+pause
+
+netsh wlan delete profile labelnet1_admins
+netsh wlan delete filter permission=allow ssid="labelnet1_admins" networktype=infrastructure
+netsh wlan show profile
+netsh wlan connect name=LABELNET2
+
+echo fin
+pause
